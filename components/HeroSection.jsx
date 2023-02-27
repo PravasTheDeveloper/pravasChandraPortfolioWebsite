8c72f3bea16069b9795bccf771dadbb07d51ab82
Pravas Chandra Sarkar <106769339+PravasTheDeveloper@users.noreply.github.com>
2023-02-27 12:35:08 +0600

    1

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -8,14 +8,17 @@ function HeroSection() {
         <>
             <div className='container mx-auto h-5/6 flex items-center'>
                 <div className='flex justify-between w-full'>
-                    <div className='text-white md:w-1/2'>
-                        <h3 className='text-3xl font-bold text-center md:text-left'>
+                    <div className='text-white md:w-1/2 w-full'>
+                        <h3 className='md:text-3xl text-xl font-bold text-center md:text-left'>
                             Hello, It's Me
                         </h3>
-                        <h1 className='text-5xl font-bold my-10 text-center md:text-left'>
+                        <h1 className='md:text-5xl text-2xl font-semibold md:font-bold my-10 text-center md:text-left '>
                             Pravas Chandra Sarkar
                         </h1>
-                        <h3 className='md:text-3xl font-bold mb-10 flex justify-center md:justify-start text-xl text-center'>
+                        <div>
+
+                        </div>
+                        <h3 className='md:text-3xl font-bold mb-10 flex justify-center md:justify-start text-lg text-center'>
                             And I'm a <span className='text-cyan-400 ml-2 text-center'><Typewriter
                                 options={{
                                     strings: ['Front-End Developer', 'React Developer','Next.js Developer','Tailwind Expert'],
@@ -25,11 +28,11 @@ function HeroSection() {
                                 }}
                             /></span>
                         </h3>
-                        <p className='text-base md:w-[500px] w-[400px] text-center md:text-left'>
+                        <p className='md:text-base text-sm font-light md:font-normal md:w-[500px] w-full px-5 text-center md:text-left'>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, ab
                             autem repellat reiciendis ipsam perspiciatis.
                         </p>
-                        <div className='text-2xl flex mt-10 justify-center md:justify-start z-[50]'>
+                        <div className='text-2xl flex mt-10 justify-center md:justify-start z-[100]'>
                             <div className='group'>
                                 <div className='w-12 h-12 border-4 border-cyan-400 group-hover:border-white duration-500 flex justify-center items-center mr-7 rounded-full'>
                                     <FaFacebookF className='text-cyan-400 group-hover:text-white' />
